3cd9ed6126df4d6243166c99160419ac064cf986
VIISilver <jparke71@hotmail.com>
2019-10-28 19:57:57 -0600

    Componentized holder text and started deal logic.

---
+++ src/components/beforeDeal/BeforeDeal.js
@@ -0,0 +1,19 @@
+import React from "react";
+import DefaultBtn from "../buttons/DefaultBtn";
+import "./BeforeDeal.css";
+
+export default function BeforeDeal(props) {
+  return (
+    <div>
+      <h2>Waiting for the Player to Press Deal...</h2>
+      <p>
+        If the deal button is not working press the shuffle button. Sends a
+        request to the server to reset the deck.
+      </p>
+      <DefaultBtn
+        callBackParent={props.shuffleDeckParent}
+        txtParent={"Shuffle Deck"}
+      />
+    </div>
+  );
+}
--- src/components/game/Game.js
+++ src/components/game/Game.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import Dealer from "../dealer/Dealer";
 import Players from "../players/Players";
 import DefaultBtn from "../buttons/DefaultBtn";
+import BeforeDeal from '../beforeDeal/BeforeDeal'
 import "./Game.css";
 
 export default class Game extends Component {
@@ -9,29 +10,67 @@ export default class Game extends Component {
     super(props);
     this.state = {
       handOpen: false,
+      playerIndexTurn: 0,
       numberOfDecks: 1,
       dealerPoints: 0,
       numberOfPlayers: 1,
       playerPoints: 0,
       cardsDealt: [],
+      allPlayersCards: [],
+      dealersCards: [],
       playerHits: 0
     };
   }
 
   newDeal = () => {
-    let openingDeal = (this.state.numberOfPlayers + 1) * 2;
-    let deckOfCardsUrl = `https://deckofcardsapi.com/api/deck/hrosz2hydqqk/draw/?count=${openingDeal}`;
+    let openingDeal = (this.state.numberOfPlayers + 1) * 2
+    let deckOfCardsUrl = `https://deckofcardsapi.com/api/deck/hrosz2hydqqk/draw/?count=${openingDeal}`
 
     function status(response) {
       if (response.status >= 200 && response.status < 300) {
-        return Promise.resolve(response);
+        return Promise.resolve(response)
       } else {
-        return Promise.reject(new Error(response.statusText));
+        return Promise.reject(new Error(response.statusText))
       }
     }
 
     function json(response) {
-      return response.json();
+      return response.json()
+    }
+
+    fetch(deckOfCardsUrl)
+      .then(status)
+      .then(json)
+      .then(data => {
+        let playerCards = []
+        let totalIteration = this.state.numberOfPlayers + 1
+        for (let i = 0; i < totalIteration; i++) {
+          playerCards.push([data.cards[i], data.cards[i + totalIteration]])
+        }
+        console.log(data.cards)
+        this.setState({
+          cardsDealt: data.cards,
+          handOpen: true
+        })
+      })
+      .catch(function(error) {
+        console.log("Request failed", error)
+      })
+  }
+
+  hitNext = () => {
+    let deckOfCardsUrl = 'https://deckofcardsapi.com/api/deck/hrosz2hydqqk/draw/?count=1'
+
+    function status(response) {
+      if (response.status >= 200 && response.status < 300) {
+        return Promise.resolve(response)
+      } else {
+        return Promise.reject(new Error(response.statusText))
+      }
+    }
+
+    function json(response) {
+      return response.json()
     }
 
     fetch(deckOfCardsUrl)
@@ -39,41 +78,41 @@ export default class Game extends Component {
       .then(json)
       .then(data =>
         this.setState({
-          cardsDealt: data.cards,
-          handOpen: !this.state.handOpen
+          cardsDealt: this.state.cardsDealt.concat(data.cards),
+          handOpen: true
         })
       )
       .catch(function(error) {
-        console.log("Request failed", error);
-      });
-  };
+        console.log("Request failed", error)
+      })
+
+  }
 
   // If you are not getting an opening deal then shuffle the deck (API Quirk)
   shuffleDeck = () => {
     fetch("https://deckofcardsapi.com/api/deck/hrosz2hydqqk/shuffle/");
   };
 
+  consoleState = () => {
+    console.log(this.state)
+  }
+
   render() {
     return (
       <div className='game-wrap'>
         <h1>Black Jack</h1>
+        <DefaultBtn
+        txtParent={'Console State'}
+        callBackParent={this.consoleState}
+        />
         <DefaultBtn
           displayBoolParent={this.state.handOpen}
           callBackParent={this.newDeal}
           txtParent={'Deal'}
         />
         {this.state.cardsDealt.length === 0 ? (
-          <div>
-            <h2>Waiting for the Player to Press Deal...</h2>
-            <p>
-              If the deal button is not working press the shuffle button. Sends
-              a request to the server to reset the deck.
-            </p>
-            <DefaultBtn
-              callBackParent={this.shuffleDeck}
-              txtParent={'Shuffle Deck'}
-            />
-          </div>
+          <BeforeDeal
+          shuffleDeckParent={this.shuffleDeck} />
         ) : (
           <div>
             <Dealer
@@ -86,6 +125,11 @@ export default class Game extends Component {
               playersCardsGame={this.state.cardsDealt.filter(
                 (item, key) => key % 2 === 0
               )}
+              hitNextGame={this.hitNext}
+            />
+            <DefaultBtn
+            callBackParent={this.hitNext}
+            txtParent={'Hit'}
             />
           </div>
         )}
